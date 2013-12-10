cabf7dec70eff22e39211ffed87c305986dcf780
Joe Pinotti <joe.pinotti@childrensmn.org>
2013-12-10 18:50:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoeskunkBlog::Application.config.secret_key_base = '6eb2e46eb73e9d7294245e2700a9ef4a3a3096499fde46d0afea057fd068e80c46c29d73b6fb06c32d10b89438b1d13009d339403f4d9993b30112778082d57e'
